d6f3fcc97797767ed2899a7331c349e0ddc8c78b
Bpost129 <Bpost129@yahoo.com>
2025-10-28 15:04:05 -0400

    change innerHtml of the newly added p element

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const titleElement = document.getElementById('main-title')
 const pElement = document.querySelector('.cool')
 
+pElement.innerHTML = "Comments for <strong>Today</strong>"
 
 
 console.dir(titleElement)

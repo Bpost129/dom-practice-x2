d49d89d14ce8173b63254121cee2f3b7ab5088b4
Bpost129 <Bpost129@yahoo.com>
2025-10-28 18:49:45 -0400

    add a list to html, use querySelectorAll to grab all items in list, use a for..of loop to modify each item

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const pElement = document.querySelector('.cool')
 const imgElement = document.getElementById('main-img')
 const linkElement = document.querySelector('a')
 
+const commentElements = document.querySelectorAll('.comment')
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
@@ -21,8 +23,13 @@ imgElement.setAttribute("alt", "A random placeholder image")
 
 linkElement.setAttribute('href', 'https://www.google.com')
 
+for (let commentElement of commentElements) {
+  commentElement.style.fontSize = '30px'
+}
+
 
 console.dir(titleElement)
 console.log(pElement)
 console.log(imgElement)
 console.log(linkElement)
+console.log(commentElements)

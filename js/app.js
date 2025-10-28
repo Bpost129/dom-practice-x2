e496de5dd067f42bb4f56a4801abf43ced1be2e9
Bpost129 <Bpost129@yahoo.com>
2025-10-28 15:10:34 -0400

    style title element, add image element

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const titleElement = document.getElementById('main-title')
 const pElement = document.querySelector('.cool')
+const imgElement = document.getElementById('main-img')
+
+titleElement.style.textAlign = 'center'
+titleElement.style.backgroundColor = 'rgb(100, 100, 300)'
+titleElement.style.color = 'rgb(150, 250, 500)'
 
 pElement.innerHTML = "Comments for <strong>Today</strong>"
 
